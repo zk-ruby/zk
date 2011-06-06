2fc520ddf2f9c0b248311c0e9ea74233f0a27a3d
slyphon <slyphon@gmail.com>
2011-06-06 20:10:52 +0000

    more docs, split some functionality out into mixin

--- lib/z_k/client.rb
+++ lib/z_k/client.rb
@@ -7,6 +7,7 @@ module ZK
   #
   class Client
     extend Forwardable
+    include ClientStateMixin
 
     DEFAULT_TIMEOUT = 10
 
@@ -107,35 +108,6 @@ module ZK
       @cnx.reopen(timeout, @event_handler.get_default_watcher_block)
       @threadpool.start!  # restart the threadpool if previously stopped by close!
       state
-    end
-
-    # Returns true if the underlying connection is in the +connected+ state.
-    def connected?
-      wrap_state_closed_error { @cnx and @cnx.connected? }
-    end
-
-    # is the underlying connection is in the +associating+ state?
-    # @return [bool]
-    def associating?
-      wrap_state_closed_error { @cnx and @cnx.associating? }
-    end
-
-    # is the underlying connection is in the +connecting+ state?
-    # @return [bool]
-    def connecting?
-      wrap_state_closed_error { @cnx and @cnx.connecting? }
-    end
-
-    # is the underlying connection is in the +expired_session+ state?
-    # @return [bool]
-    def expired_session?
-      return nil unless @cnx
-
-      if defined?(::JRUBY_VERSION)
-        @cnx.state == Java::OrgApacheZookeeper::ZooKeeper::States::EXPIRED_SESSION
-      else
-        wrap_state_closed_error { @cnx.state == Zookeeper::ZOO_EXPIRED_SESSION_STATE }
-      end
     end
 
     # does a stat on '/', rescues all zookeeper-protocol exceptions
