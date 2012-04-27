996fa215b0e2b2091230edeaeec5075dde83b74c
Jonathan D. Simms <slyphon@gmail.com>
2012-04-27 05:09:05 +0000

    woohoo! figured out how to handle close! when called from a threadpool thread.
    See, I'd totally forgotten that there are *other threads*,
    and you can pretty much just *create new ones* when you want to!
    
    So shutdown now works like this:
    
    * spin up a new shutdown thread
    
    * inside that shutdown thread, do all the stuff we used to do:
      shutting down the threadpool, then closing the client connection
    
    * in the calling thread, if we're in a threadpool thread, then just
      exit close! so that the shutdown thread can join() us. otherwise,
      join the shutdown thread and wait for it to exit (just like how it
      used to work, at least in terms of waiting-for-close).
    
    This commit also fixes a few related things with the MessageQueue, which
    was apparently not cleaning up after itself during destroy! enough.

--- spec/zk/client_spec.rb
+++ spec/zk/client_spec.rb
@@ -3,5 +3,41 @@ require 'spec_helper'
 describe ZK::Client::Threaded do
   include_context 'threaded client connection'
   it_should_behave_like 'client'
+
+  describe :close! do
+    describe 'from a threadpool thread' do
+      it %[should do the right thing and not fail] do
+        # this is an extra special case where the user obviously hates us
+
+        @zk.should be_kind_of(ZK::Client::Threaded) # yeah yeah, just be sure
+
+        mutex = Mutex.new
+        enter_cond = ConditionVariable.new
+        exit_cond = ConditionVariable.new
+
+        @zk.defer do
+          mutex.synchronize do
+#             logger.debug { "waiting for signal to enter" }
+#             enter_cond.wait(mutex)
+            begin
+              @zk.close!
+            ensure
+              logger.debug { "signaling on exit" }
+              exit_cond.signal
+            end
+          end
+        end
+
+        mutex.synchronize do
+#           logger.debug { "signalling threadpool thread to exit" }
+#           enter_cond.signal
+          logger.debug { "waiting for exit" }
+          exit_cond.wait(mutex)
+        end
+
+        wait_until { @zk.closed? }.should be_true 
+      end
+    end
+  end
 end
 
