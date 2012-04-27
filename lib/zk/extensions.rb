22d0de029c19bed77c530a05f6e8ae831b7b9d87
Jonathan D. Simms <slyphon@gmail.com>
2012-04-27 02:37:33 +0000

    copy ActiveSupport::Concern into ZK namespace (from 3.2.3)
    I want to use this, and don't want it to conflict if someone uses ZK as part of a rails app.

--- lib/zk/extensions.rb
+++ lib/zk/extensions.rb
@@ -1,4 +1,36 @@
 module ZK
+  # this is taken from activesupport-3.2.3, and pasted here so that we don't conflict if someone
+  # is using us as part of a rails app
+  #
+  # i've removed the code that includes InstanceMethods (tftfy)
+  # @private
+  module Concern
+    def self.extended(base)
+      base.instance_variable_set("@_dependencies", [])
+    end
+
+    def append_features(base)
+      if base.instance_variable_defined?("@_dependencies")
+        base.instance_variable_get("@_dependencies") << self
+        return false
+      else
+        return false if base < self
+        @_dependencies.each { |dep| base.send(:include, dep) }
+        super
+        base.extend const_get("ClassMethods") if const_defined?("ClassMethods")
+        base.class_eval(&@_included_block) if instance_variable_defined?("@_included_block")
+      end
+    end
+
+    def included(base = nil, &block)
+      if base.nil?
+        @_included_block = block
+      else
+        super
+      end
+    end
+  end
+
   module Extensions
     # some extensions to the ZookeeperCallbacks classes, mainly convenience
     # interrogators
