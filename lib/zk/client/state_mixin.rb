bd789ba9ffe23219edef1dc3003a27282f506dfe
slyphon <slyphon@gmail.com>
2012-05-31 22:36:40 +0000

    clear out cruft, let's make this 1.6.2

--- lib/zk/client/state_mixin.rb
+++ lib/zk/client/state_mixin.rb
@@ -3,51 +3,6 @@ module ZK
     # Provides client-state related methods. Included in ZK::Client::Base.
     # (refactored out to this class to ease documentation overload)
     module StateMixin
-#       # Returns true if the underlying connection is in the +connected+ state.
-#       def connected?
-#         wrap_state_closed_error { cnx and cnx.connected? }
-#       end
-
-#       # is the underlying connection is in the +associating+ state?
-#       # @return [bool]
-#       def associating?
-#         wrap_state_closed_error { cnx and cnx.associating? }
-#       end
-
-#       # is the underlying connection is in the +connecting+ state?
-#       # @return [bool]
-#       def connecting?
-#         wrap_state_closed_error { cnx and cnx.connecting? }
-#       end
-#       
-#       # is the underlying connection is in the +expired_session+ state?
-#       # @return [bool]
-#       def expired_session?
-#         return nil unless @cnx
-
-#         if defined?(::JRUBY_VERSION)
-#           cnx.state == Java::OrgApacheZookeeper::ZooKeeper::States::EXPIRED_SESSION
-#         else
-#           wrap_state_closed_error { cnx.state == Zookeeper::ZOO_EXPIRED_SESSION_STATE }
-#         end
-#       end
-
-      # returns the current state of the connection as reported by the underlying driver
-      # as a symbol. The possible values are <tt>[:closed, :expired_session, :auth_failed
-      # :connecting, :connected, :associating]</tt>. 
-      #
-      # See the Zookeeper session 
-      # {documentation}[http://hadoop.apache.org/zookeeper/docs/current/zookeeperProgrammers.html#ch_zkSessions]
-      # for more information
-      #
-#       def state
-#         if defined?(::JRUBY_VERSION) 
-#           cnx.state.to_string.downcase.to_sym
-#         else
-#           STATE_SYM_MAP.fetch(cnx.state) { |k| raise IndexError, "unrecognized state: #{k}" }
-#         end
-#       end
-
       # Register a block to be called when *any* connection event occurs
       #
       # @yield [event] yields the connection event to the block
