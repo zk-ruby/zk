6da47f8eeedc9b8c029525903f4edf20a2b7f72a
Andreas Kotes <berlincount@users.noreply.github.com>
2018-01-11 12:06:14 +0100

    use current Ruby versions for testing; cut some unused test routines per recommendations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -60,50 +60,6 @@ RSpec.configure do |config|
       SpecGlobalLogger.logger.debug  { "stopping zookeeper service" }
       ZK::Server.shutdown
     end
-
-    config.before(:each, zookeeper: true) do
-      ZK.open("#{ZK.default_host}:#{ZK.test_port}") do |zk|
-        zk.rm_rf("/test")
-        zk.mkdir_p("/test")
-      end
-    end
-
-    config.before(:each, proxy: true) do |group|
-      proxy_start(group.metadata[:throttle_bytes_per_sec])
-    end
-
-    config.after(:each, proxy: true) do
-      proxy_stop
-    end
-
-    def proxy_start(throttle_bytes_per_sec = nil)
-      limit = "-L #{throttle_bytes_per_sec}" if throttle_bytes_per_sec
-      spawn(%{socat -T 10 -d TCP-LISTEN:#{ZK.test_proxy_port},fork,reuseaddr,linger=1 SYSTEM:'pv -q #{limit} - | socat - "TCP:localhost:#{ZK.test_port}"'})
-    end
-
-    def proxy_stop
-      system("lsof -i TCP:#{ZK.test_proxy_port} -t | grep -v #{Process.pid} | xargs kill -9")
-    end
-
-    def spawn(cmd)
-      puts "+ #{cmd}" if ENV['ZK_DEBUG']
-      Kernel.spawn(cmd)
-    end
-
-    def system(cmd)
-      puts "+ #{cmd}" if ENV['ZK_DEBUG']
-      Kernel.system(cmd)
-    end
-
-    def almost_there(tries = 100)
-      i ||= 0
-      yield
-    rescue RSpec::Expectations::ExpectationNotMetError
-      raise if i > tries
-      sleep 0.1
-      i += 1
-      retry
-    end
   end
 
   # tester should return true if the object is a leak
